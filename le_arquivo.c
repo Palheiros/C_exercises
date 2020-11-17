6bdf61392fe046496ecc374054403159cb485dd0
Ricardo Palheiros <47124346+Palheiros@users.noreply.github.com>
2020-11-17 18:05:05 -0300

    Update le_arquivo.c

--- le_arquivo.c
+++ le_arquivo.c
@@ -1,4 +1,5 @@
-// Programa que abre um arquivo de texto, lê cada caractere e faz o output no terminal
+// Programa que abre um arquivo de texto, lÃª cada caractere e faz o output no terminal
+// VocÃª precisa ter um aquivos chamado notas.txt na mesma pasta do programa ou modificar a linha 16 com o nome do arquivo que deseja
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -15,8 +16,8 @@ int main(){
     file = fopen("notas.txt", "r"); // indica qual arquivo abrir. O 'r' significa 'read' (ler)
 
     if (file){
-        while ((c = getc(file)) != EOF){    // vai buscar no arquivo caracter por caracter enquanto não chegar no final (EOF = End of File)
-            printf("%c", c);    // imprime na tela cada caractere que lê e atribui na variável 'c'
+        while ((c = getc(file)) != EOF){    // vai buscar no arquivo caracter por caracter enquanto nÃ£o chegar no final (EOF = End of File)
+            printf("%c", c);    // imprime na tela cada caractere que lÃª e atribui na variÃ¡vel 'c'
         }
     }
 
